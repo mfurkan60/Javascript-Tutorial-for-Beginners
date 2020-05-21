6c7afb62319124679651cb54d30204a5977fbc16
Mert Furkan Erguden <furfur60@hotmail.com>
2020-05-21 03:34:21 +0300

    update files
    update files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Javascript-Tutorial-for-Beginners
-Javascript Tutorial for Beginners
+JavaScript is the programming language of HTML and the Web.
+[W3schools Tutorial ](https://www.w3schools.com/js/default.asp)
+JavaScript is easy to learn programing language.
+
+In this  tutorial will show some examples of  JavaScript Programing Language from **basic to advanced level.**
+I have divided some topics:
+ 1. What is Js?
+ 2. Variables
+ 3. Strings
+ 4. Arrays
+ 5. Loops
+ 6. Switch-Case
+ 7. Function
+
+
+
+
---
+++ Var,Let,Const/app.js
@@ -0,0 +1,46 @@
+// -----Var ----- 
+
+/* var name = "Alex";
+ console.log(name);
+
+ name = "Alex";
+
+console.log(name);*/
+
+//------ Let--------
+
+/*let a,b;
+
+ a = 10;
+ b = 20; you can implemantation value of a or b
+
+ console.log(a+b);*/
+
+//---------Const----------
+
+// const name = "Alex";
+
+// console.log(name);
+
+// name = "Lucy";   you can not change value so ıts getting error
+
+// console.log(name);
+
+const a = [1,2,3,4,5]; 
+
+console.log(a);
+
+a.push(6); //push up 6 to array
+a.pop(); // pop up to array
+
+const b = 10;
+
+b = 20;
+
+console.log(a);
+
+
+
+
+
+
